7f9926beb04f88b6840f7ed3d7caba755ec6c311
roshansj <roshansjames@gmail.com>
2023-01-28 20:43:18 -0500

    completed

--- add_2_numbers.py
+++ add_2_numbers.py
@@ -1,4 +1,15 @@
-a=2
-b = 3
-sum = a+b
-print(sum)
+nums = [2,7,11,15]
+target = 26
+
+a = 0
+b = 0
+
+len_nums = len(nums)
+
+for x in range(0,len_nums):
+    a = nums[x]
+    for y in range(x,len_nums):
+        b = nums[y]
+        if a + b == target:
+            print(x,y)
+
