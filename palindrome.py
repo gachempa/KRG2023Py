b2e15286b8670b7aed951c47ce86d5cb3f6697be
roshansj <roshansjames@gmail.com>
2023-01-28 21:23:26 -0500

    completed

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,8 @@
-int_number = 12345
+int_number = 12321
 res = [int(x) for x in str(int_number)]
-resversed_res = res.reverse()
-print(resversed_res)
+reverse_res = res.copy()
+reverse_res.reverse()
+if res == reverse_res:
+    print("true")
+else:
+    print("false")
