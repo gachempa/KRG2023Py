7b9edab703f81a9ed1653192bf6252f8eb2d82b2
gachempa <jpsg514@gmail.com>
2023-01-29 19:26:12 -0800

    learned sonme more on column argsort

--- numpy_skyline.py
+++ numpy_skyline.py
@@ -8,16 +8,27 @@ np_buildings = np.array(buildings)
 
 print(np_buildings)
 
-print("After sorting on last axis: ")
-print(np.sort(np_buildings))
+# print("After sorting on last axis: ")
+# print(np.sort(np_buildings))
 
-print("After sorting on axis = 0: ")
-print(np.sort(np_buildings, axis=0))
+# print("After sorting on axis = 0: ")
+# print(np.sort(np_buildings, axis=0))
 
-print("After sorting: ")
-print(np_buildings)
+print("After arg-sorting on axis = 0: ")
+print(np_buildings[np_buildings[:,1].argsort()])
+
+# print("After sorting: ")
+# print(np_buildings)
 print("shape is", np.shape(np_buildings))
-print("The second row is", np_buildings[1,:])
+# print("The second row is", np_buildings[1,:])
 print("dimension is", np.ndim(np_buildings))
 print("minimum value is", np.min(np_buildings))
 print("sum of each row is", np.sum(np_buildings, axis=0))
+
+np_buildings_2 = np.copy(np_buildings)
+np_buildings_2[0,1] = 99
+print("np2 is:")
+print(np_buildings_2)
+# print(np_buildings==np_buildings_2)
+list_np = np_buildings.tolist()
+print("this is list_np", list_np)
