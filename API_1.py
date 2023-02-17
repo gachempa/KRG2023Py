a46f774d9e1f9628c9d89da6f9007e420f2ee656
gachempa <jpsg514@gmail.com>
2023-02-16 22:07:08 -0800

    test api

--- API_1.py
+++ API_1.py
@@ -3,4 +3,4 @@ import requests
 response = requests.get("http://api.open-notify.org/astros.json")
 print(response.status_code)
 print(response.json())
-print("Well - it's a start....... ....!")
+print("Well - it's a start...!")
