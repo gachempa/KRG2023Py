d9b915ed55cfde9b5ab92bf79347737c46bd29dd
gachempa <102880333+gachempa@users.noreply.github.com>
2023-04-14 18:17:18 -0700

    Create Shellgameusacoinput.py

---
+++ Shellgameusacoinput.py
@@ -0,0 +1,47 @@
+
+import sys
+
+sys.stdin = open("shell.in")
+
+sys.stdout = open("shell.out", "w")
+
+n_swaps = map(int, input())
+
+swaps=[]
+
+for x in range n_swaps:
+  swaps[x] = map(int, input().split())
+
+position_abc = [[1,0,0],[0,1,0], [0,0,1]] 
+
+score_a = 0
+score_b = 0
+score_c = 0
+
+def doswap(a,b):
+#    print(position_abc)
+    x=position_abc[0][a-1]
+    y=position_abc[1][a-1]
+    z=position_abc[2][a-1]    
+    position_abc[0][a-1]=position_abc[0][b-1]
+    position_abc[0][b-1]=x
+
+    position_abc[1][a-1]=position_abc[1][b-1]
+    position_abc[1][b-1]=y
+
+    position_abc[2][a-1]=position_abc[2][b-1]
+    position_abc[2][b-1]=z
+
+for x in range(n_swaps):
+    var_x = swaps[x][0]
+    var_y = swaps[x][1]
+    choice = swaps[x][2]    
+
+    doswap(var_x,var_y)
+
+    if position_abc[0][choice-1] == 1: score_a += 1
+    if position_abc[1][choice-1] == 1: score_b += 1
+    if position_abc[2][choice-1] == 1: score_c += 1
+
+print(max(score_a, score_b,score_c)) 
+
