fbc74e406638d434712cd80dd7387e3067670f33
roshansj <roshansjames@gmail.com>
2023-04-16 16:52:11 -0400

    added code

---
+++ swap2numbers.py
@@ -0,0 +1,11 @@
+numbers = [2,3]
+temp = numbers[0]
+numbers[0] = numbers[1]
+numbers[1] = temp
+print(numbers)
+# numbers_copy = numbers
+# numbers[0] = numbers_copy[1]
+# print(numbers)
+# print(numbers_copy)
+# numbers[1] = numbers_copy[0]
+# print(numbers)
