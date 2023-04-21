389254a1879d5b6a92eaa91e9c7e76399570e054
gachempa <jpsg514@gmail.com>
2023-04-20 21:08:38 -0700

    Complete

--- maxDistance.py
+++ maxDistance.py
@@ -1,1 +1,18 @@
 
+# usaco input lines - works
+# import sys
+
+n = int(input(""))
+n_x = [int(x) for x in input().split()]
+n_y = [int(x) for x in input().split()]
+
+maxdistance = 0
+
+for x in range(n-1):
+    for y in range(x+1,n):
+        distxsq=(n_x[y]-n_x[x])**2
+        distysq=(n_y[y]-n_y[x])**2
+        distsq=distxsq+distysq
+        maxdistance=max(maxdistance,distsq)
+        
+print(maxdistance)
