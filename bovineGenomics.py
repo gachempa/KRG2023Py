defa4066ff7fc7e372cbf06e9deb732dc7488c50
gachempa <jpsg514@gmail.com>
2023-05-01 21:32:29 -0700

    Started programming

--- bovineGenomics.py
+++ bovineGenomics.py
@@ -1,1 +1,23 @@
+# usaco input lines - testing
+# import sys
+
+# n, m = map(int, input().split())
+
+n = 3
+m = 8
+
+m_spottyCows = []
+m_plainCows = []
+
+input_1 = AATCCCAT
+input_2 = GATTGCAA
+input_3 = GGTCGCAA
+input_4 = ACTCCCAG
+input_5 = ACTCGCAT
+input_6 = ACTTCCAT
+
+for x in range(0, 2*n):
+    
+
+
 
