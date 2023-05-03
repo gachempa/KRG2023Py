6222da086d8f229bd42e72fc9aa2b184dd559265
gachempa <jpsg514@gmail.com>
2023-05-02 19:58:06 -0700

    halfway there

--- bovineGenomics.py
+++ bovineGenomics.py
@@ -11,11 +11,17 @@ m_plainGenome = []
 
 bovine_genomes= ["AATCCCAT","GATTGCAA","GGTCGCAA","ACTCCCAG","ACTCGCAT","ACTTCCAT"]
 
-print(bovine_genomes[0][0])
+# print(bovine_genomes[0][0])
 
-for x in range(0, 2*n):
-    if x<n:
-        
+for y in range(0,m):
+    print("--------")
+    for x in range(0, 2*n):
+        if x<n:
+            # print(bovine_genomes[x][y])
+            # print("---")
+            m_spottyGenome.append(bovine_genomes[x][y])
+        if x>=n:
+            m_plainGenome.append(bovine_genomes[x][y])
 
-
-
+    print(m_spottyGenome)
+    print(m_plainGenome)
