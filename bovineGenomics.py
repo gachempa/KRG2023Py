cfa4e17b861df6819cfaf37de4fd315b63d08e1b
gachempa <jpsg514@gmail.com>
2023-05-02 22:56:41 -0700

    not done

--- bovineGenomics.py
+++ bovineGenomics.py
@@ -8,20 +8,29 @@ m = 8
 
 m_spottyGenome = []
 m_plainGenome = []
-
+locationscore = 0
 bovine_genomes= ["AATCCCAT","GATTGCAA","GGTCGCAA","ACTCCCAG","ACTCGCAT","ACTTCCAT"]
 
-# print(bovine_genomes[0][0])
+def mutation(listspotty, listplain, ncows):
+    i=0
+    for x in range(0, ncows):
+        if listspotty[x] in listplain:
+            print("xxxx")
+            print(listspotty[x])
+            print("xxxx")
+            break
+        
+        else: i = 1
+    return i
 
 for y in range(0,m):
-    print("--------")
-    for x in range(0, 2*n):
-        if x<n:
-            # print(bovine_genomes[x][y])
-            # print("---")
-            m_spottyGenome.append(bovine_genomes[x][y])
-        if x>=n:
-            m_plainGenome.append(bovine_genomes[x][y])
+    m_spottyGenome.clear()
+    m_plainGenome.clear()       
+    for x in range(0, n):
+        m_spottyGenome.append(bovine_genomes[x][y])
+        m_plainGenome.append(bovine_genomes[n+x][y])
+        locationscore = locationscore + mutation(m_spottyGenome, m_plainGenome, n)
 
     print(m_spottyGenome)
     print(m_plainGenome)
+print(locationscore)
