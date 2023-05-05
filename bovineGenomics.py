067b81af2ed8eb955ec9a3fd89e31d772eb5aa73
gachempa <jpsg514@gmail.com>
2023-05-04 20:17:09 -0700

    completed - adjust for usaca

--- bovineGenomics.py
+++ bovineGenomics.py
@@ -11,12 +11,15 @@ m_plainGenome = []
 locationscore = 0
 bovine_genomes= ["AATCCCAT","GATTGCAA","GGTCGCAA","ACTCCCAG","ACTCGCAT","ACTTCCAT"]
 
-def mutation(listspotty, listplain, ncows):
-    for x in range(0, 1):
-        print("xxxx")
-        print(listspotty)
-        print("xxx----xxx")
-    return 1
+def mutation(listspotty, listplain, ncows, n_position):
+    matches = ncows
+    for x in range(0, ncows):
+        if listspotty[x] not in listplain:
+            matches = matches - 1
+    if matches == 0: 
+        print("position with no genome match: ", n_position, listspotty, listplain)
+        return 1
+    else: return 0
 
 for y in range(0,m):
     m_spottyGenome.clear()
@@ -25,6 +28,6 @@ for y in range(0,m):
         m_spottyGenome.append(bovine_genomes[x][y])
         m_plainGenome.append(bovine_genomes[n+x][y])
         if x == n-1:
-            locationscore = locationscore + mutation(m_spottyGenome, m_plainGenome, n)
+            locationscore = locationscore + mutation(m_spottyGenome, m_plainGenome, n, x)
 
 print(locationscore)
